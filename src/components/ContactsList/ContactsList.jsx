301bfb0673db7bc68029f52aa6e0e49604fb5000
Fyl Andrii <fil.andrey91@gmail.com>
2022-12-13 10:27:03 +0200

    deletes old code

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import css from './ContactsList.module.css';
-// import PropTypes from 'prop-types';
-
-// const ContactsList = ({ contacts, onDeleteContact }) => {
-//   return (
-//     <ul className={css.list__container}>
-//       {/* Деструктуризируем contacts -> id, name */}
-//       {contacts.map(({ id, name, phone }) => (
-//         <li className={css.item} key={id}>
-//           <p className={css.name}>{name}</p>
-//           <p className={css.number}>{phone}</p>
-//           <button
-//             className={css.btn__delete}
-//             type="button"
-//             onClick={() => onDeleteContact(id)}
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactsList;
-
-// ContactsList.propTypes = {
-//   contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   onDeleteContact: PropTypes.func.isRequired,
-// };
-
 import React from 'react';
 import css from './ContactsList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
